9308d0b94b354bcb34d853ecf52c6009e1f48da8
Charles Butler <cdbutle@outlook.com>
2021-07-29 20:43:42 -0500

    added solution

---
+++ Python_practice/python_factors.py
@@ -0,0 +1,16 @@
+def print_factors(x, p):
+  arr=[]
+
+
+  print("The factors of",x,"are:")
+   
+  for i in range(1, x + 1):
+       if x % i == 0:
+         arr.append(i)
+
+    print(arr, p)
+    print(arr[p-1])
+
+num = 320
+p = 1
+print_factors(num, p)
