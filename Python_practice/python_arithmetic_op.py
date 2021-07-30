ae40e710387c1b6f081ed1e45aa13ea3a0c77888
Charles Butler <cdbutle@outlook.com>
2021-07-29 21:14:06 -0500

    final solution

---
+++ Python_practice/python_arithmetic_op.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    
+    #first line is sum of 2 numbers
+    #2second line is difference of 2 numbers
+    #third line is product of 2 numbers
+    
+    print a+b
+    print a-b
+    print a*b
